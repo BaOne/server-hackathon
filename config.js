155d689cce578c9bf30e3e763ac5b3911f88e987
BaOne <baoan.nguyen10@gmail.com>
2016-04-06 20:42:16 +0200

    add config documentdb
    config documentdb

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-module.exports = {
-  app: {
-    port: 9999
-  },
-  db: {
-    host: '',
-    masterKey: '',
-    name: ''
-  }
-};
+var config = {}
+
+config.host = process.env.HOST || "https://team18-axasafe.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "UkNBAemuQOBpQPc6sP90vQq4Bg7OxhwmmxRVjWZJHzuHnMQu6JmK6TVTsYQpdQnyza5BahkiH8OVsbqTWPcEcw==";
+config.databaseId = "List";
+config.collectionId = "Items";
+
+module.exports = config;
